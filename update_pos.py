1dae1b09f683f7d77143b4c5275e1c4b1a2590d3
Miao-cc <1033570101@qq.com>
2021-06-06 12:53:03 +0800

    update the update_pos.py

--- update_pos.py
+++ update_pos.py
@@ -1,12 +1,17 @@
-import fitsio
+#import fitsio
+from astropy.io.fits import update
+from astropy.io import fits as pyfits
 import os, sys
 
 filename = sys.argv[1]
 ra = sys.argv[2]
 dec = sys.argv[3]
 
-hdulist = fitsio.FITS(filename, 'rw')
-hdulist[0].write_key('RA',  ra)
-hdulist[0].write_key('DEC', dec)
+hdulist = pyfits.open(filename, mode='update')
+hd0 = hdulist[0]
+hd0.header['RA'] = ra
+hd0.header['DEC'] = dec
+print(hd0.header['RA'])
+print(hd0.header['DEC'])
 
 hdulist.close()
