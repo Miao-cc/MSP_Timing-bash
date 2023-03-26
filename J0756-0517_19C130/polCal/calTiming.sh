1a02818ec7186623d7f6e4e5dc53b9e194d64a30
Mcc <1033570101@qq.com>
2023-03-27 00:25:36 +0800

    cal database

---
+++ J0756-0517_19C130/polCal/calTiming.sh
@@ -0,0 +1,17 @@
+#polCal.psh -e ucf 19C130_20220830Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20220901Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20220903Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20220904Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20220905Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20220907Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20220912Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20220915Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20220920Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20221007Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20221022Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20221030Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20221213Cal1.ar.pazi
+#polCal.psh -e ucf 19C130_20230115Cal1.ar.pazi
+polCal.psh -e ucf 19C130_20230323Cal1.ar.pazi
+
+pac -u ucf -w -k 19C130Cal.txt
