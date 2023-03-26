3bc73269bd691dbd0172d98da5e2c074ffd3b16e
Mcc <1033570101@qq.com>
2023-03-27 00:24:46 +0800

    pulsar pol calib

---
+++ J0756-0517_19C130/allFold/psrPolCal.sh
@@ -0,0 +1,58 @@
+#psrHeader.psh -m J2027+0255_20190915Timing.ar.pazi
+#pac -d ../polCal/19C63Cal.txt -n q -c -Z -U -e pazi.calib J2027+0255_20190915Timing.ar.pazi
+#zap.psh -m J2027+0255_20210228Timing.ar.pazi.calibP
+
+
+#psrHeader.psh -m J0756-0517_20220830Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20220901Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20220903Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20220904Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20220905Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20220907Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20220912Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20220912Timing_0002.ar.pazi
+#psrHeader.psh -m J0756-0517_20220915Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20220920Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20221007Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20221022Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20221030Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20221213Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20230115Timing_0001.ar.pazi
+#psrHeader.psh -m J0756-0517_20230323Timing_0001.ar.pazi
+  
+
+
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20220830Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20220901Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20220903Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20220904Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20220905Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20220907Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20220912Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20220912Timing_0002.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20220915Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20220920Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20221007Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20221022Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20221030Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20221213Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20230115Timing_0001.ar.pazi
+#pac -d ../polCal/19C130Cal.txt -n q -c -Z -U -e pazi.calib J0756-0517_20230323Timing_0001.ar.pazi
+
+
+#zap.psh -m J0756-0517_20220830Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20220901Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20220903Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20220904Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20220905Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20220907Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20220912Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20220912Timing_0002.ar.pazi.calibP
+#zap.psh -m J0756-0517_20220915Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20220920Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20221007Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20221022Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20221030Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20221213Timing_0001.ar.pazi.calibP
+#zap.psh -m J0756-0517_20230115Timing_0001.ar.pazi.calibP
+zap.psh -m J0756-0517_20230323Timing_0001.ar.pazi.calibP
